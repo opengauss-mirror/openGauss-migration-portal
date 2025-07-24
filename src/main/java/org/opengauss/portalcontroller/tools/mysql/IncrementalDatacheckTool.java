51b53f9f9dc4477160dc3e6c740d61feabea04f5
duanguoqiang <duanguoqiang4@huawei.com>
2025-07-24 15:46:11 +0800

    去除在线迁移模式增量校验流程

--- src/main/java/org/opengauss/portalcontroller/tools/mysql/IncrementalDatacheckTool.java
+++ src/main/java/org/opengauss/portalcontroller/tools/mysql/IncrementalDatacheckTool.java
@@ -18,8 +18,6 @@ import org.opengauss.portalcontroller.constant.Check;
 import org.opengauss.portalcontroller.constant.Debezium;
 import org.opengauss.portalcontroller.constant.Method;
 import org.opengauss.portalcontroller.constant.Parameter;
-import org.opengauss.portalcontroller.constant.Status;
-import org.opengauss.portalcontroller.logmonitor.listener.LogFileListener;
 import org.opengauss.portalcontroller.task.Plan;
 import org.opengauss.portalcontroller.task.Task;
 import org.opengauss.portalcontroller.utils.InstallMigrationUtils;
@@ -29,7 +27,6 @@ import org.opengauss.portalcontroller.utils.ProcessUtils;
 import org.opengauss.portalcontroller.utils.PropertitesUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -91,31 +88,8 @@ public class IncrementalDatacheckTool extends FullDatacheckTool {
     public boolean start(String workspaceId) {
         fileCheck.startCheck();
         Task.startDataCheck(fileCheck.getCheckResultListener());
-        incrementMigrationResumeBrokenTransfer();
         stop();
         return true;
-    }
-
-    private void incrementMigrationResumeBrokenTransfer() {
-        String name = "";
-        while (!Plan.stopPlan && !Plan.stopIncrementalMigration) {
-            LOGGER.info("Incremental migration is running... {}", Plan.runIncrementalMigrationEndpoint);
-            if (StringUtils.hasLength(Plan.runIncrementalMigrationEndpoint)) {
-                name = Plan.runIncrementalMigrationEndpoint;
-                Plan.runIncrementalMigrationEndpoint = "";
-                LOGGER.info("resume broken transfer of incremental migration endpoint: {}", name);
-                startConnectMigrationEndpoint(name);
-                Plan.pause = false;
-            }
-            ProcessUtils.sleepThread(1000, "running incremental migration");
-        }
-    }
-
-    private void startConnectMigrationEndpoint(String connectMigrationEndpoint) {
-        LOGGER.info("incrementMigrationResumeBrokenTransfer start task  {}", connectMigrationEndpoint);
-        Task.startTaskMethod(connectMigrationEndpoint, 5000, "", new LogFileListener());
-        PortalControl.status = Status.RUNNING_INCREMENTAL_MIGRATION;
-        Plan.pause = false;
     }
 
     @Override
